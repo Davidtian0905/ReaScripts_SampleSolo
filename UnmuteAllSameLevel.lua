b9e325d51d847aeb808f88024dfe41ad481e9a01
David Tian <122532842+Davidtian0905@users.noreply.github.com>
2024-07-04 09:49:52 +0800

    Update UnmuteAllSameLevel.lua

--- UnmuteAllSameLevel.lua
+++ UnmuteAllSameLevel.lua
@@ -3,35 +3,24 @@
 -- @B站 https://space.bilibili.com/538201523/
 -- @Github https://github.com/Davidtian0905/ReaScripts_SampleSolo
 
-function UnmuteAllTracksAtSameLevel()
+function UnmuteAllTracksAtSameParentLevel()
   local selectedTrack = reaper.GetSelectedTrack(0, 0)
   if not selectedTrack then return end
   
-  local selectedTrackDepth = GetTrackDepth(selectedTrack)
+  local selectedTrackParent = reaper.GetParentTrack(selectedTrack)
   
   reaper.Undo_BeginBlock()
-
   local trackCount = reaper.CountTracks(0)
   
   for i = 0, trackCount - 1 do
     local checkTrack = reaper.GetTrack(0, i)
-    if GetTrackDepth(checkTrack) == selectedTrackDepth then
-      reaper.SetMediaTrackInfo_Value(checkTrack, "B_MUTE", 0)  -- Unmute all tracks at the same level
+    if reaper.GetParentTrack(checkTrack) == selectedTrackParent then
+      reaper.SetMediaTrackInfo_Value(checkTrack, "B_MUTE", 0) -- Unmute all tracks with the same parent
     end
   end
   
-  reaper.Undo_EndBlock("Unmute All Tracks at Same Level", -1)
+  reaper.Undo_EndBlock("Unmute All Tracks at Same Parent Level", -1)
   reaper.UpdateArrange()
 end
 
-function GetTrackDepth(track)
-  local depth = 0
-  local parent = reaper.GetParentTrack(track)
-  while parent do
-    depth = depth + 1
-    parent = reaper.GetParentTrack(parent)
-  end
-  return depth
-end
-
-UnmuteAllTracksAtSameLevel()
+UnmuteAllTracksAtSameParentLevel()
